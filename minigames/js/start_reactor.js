90dcb5991e3fcdd39af1a058662d505372e9efe7
3lCR3P3R <3lCR3P3R@users.noreply.github.com>
2020-10-25 23:25:27 -0500

    Creacion y ubicacion de los orbes

--- minigames/js/start_reactor.js
+++ minigames/js/start_reactor.js
@@ -4,7 +4,32 @@ var ctx = canvas.getContext('2d');
 ctx.fillStyle = "rgb(56,137,210)";
 
 var imagenFondo = new Image();
+var imgOrbeVerde = new Image();
+var imgOrbeRojo = new Image();
 imagenFondo.src = "https://i.imgur.com/40xaJKe.png";
+imgOrbeVerde.src = "https://i.imgur.com/ssj9GCl.png";
+imgOrbeRojo.src = "https://i.imgur.com/Qd0KuYo.png";
+
+function orbeColor(orbe, posX, posY, ancho, alto) {
+    this.posX = posX;
+    this.posY = posY;
+    this.ancho = ancho;
+    this.alto = alto;
+}  
+
+var orbeVerdeDos = new rect(178, 92, 54, 53);
+var orbeVerdeTres = new rect(262, 92, 54, 53);
+var orbeVerdeCuatro = new rect(344, 92, 54, 53);
+var orbeVerdeCinco = new rect(424, 92, 54, 53);
+let orbesVerdes = [orbeVerdeDos, orbeVerdeTres, orbeVerdeCuatro, orbeVerdeCinco];
+
+var orbeRojoUno = new rect(711, 92, 54, 53);
+var orbeRojoDos = new rect(792, 92, 54, 53);
+var orbeRojoTres = new rect(873, 92, 54, 53);
+var orbeRojoCuatro = new rect(958, 92, 54, 53);
+var orbeRojoCinco = new rect(1038, 92, 54, 53);
+let orbesRojos = [orbeRojoUno, orbeRojoDos, orbeRojoTres, orbeRojoCuatro, orbeRojoCinco];
+
 
 function rect(posX, posY, ancho, alto) {
     this.posX = posX;
@@ -22,13 +47,19 @@ var cuadroSeis = new rect(358, 348, 100, 100);
 var cuadroSiete = new rect(138, 458, 100, 100);
 var cuadroOcho = new rect(248, 458, 100, 100);
 var cuadroNueve = new rect(358, 458, 100, 100);
-let cuadros = [cuadroUno, cuadroDos, cuadroTres, cuadroCuatro, cuadroCinco, cuadroSeis, cuadroSiete, cuadroOcho, cuadroNueve]
+let cuadros = [cuadroUno, cuadroDos, cuadroTres, cuadroCuatro, cuadroCinco, cuadroSeis, cuadroSiete, cuadroOcho, cuadroNueve];
 
 window.onload = () => {
     cuadros.forEach(cuadro => {
         pintarRectangulo(cuadro);
     });
     pintarImagenDeFondo();
+    orbesRojos.forEach(orbe => {
+        pintarOrbes(imgOrbeRojo, orbe);
+    });
+    orbesVerdes.forEach(orbe => {
+        pintarOrbes(imgOrbeVerde, orbe);
+    });
 }
 
 function limpiarTablero(){
@@ -41,4 +72,8 @@ function pintarImagenDeFondo(){
 
 function pintarRectangulo({ posX, posY, ancho, alto}){
     ctx.fillRect(posX, posY, ancho, alto);
+}
+
+function pintarOrbes(orbe, {posX, posY, ancho, alto}){
+    ctx.drawImage(orbe, posX, posY, ancho, alto);
 }
